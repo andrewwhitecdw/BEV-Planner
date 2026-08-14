fix: remove unused accelation computation in inner_forward
Signed-off-by: andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>

mmdet3d/models/fbbev/planner_head/AD_mlp.py
@@ -220,9 +220,7 @@ class AD_MLP(Base3DDetector):
         # gt_ego_fut_trajs =  torch.stack(kwargs['gt_ego_fut_trajs'], 0)
         self.ego_fut_steps = 6
 
-        vel = gt_ego_lcf_feat[:, :2].unsqueeze(1).repeat(1, self.ego_fut_steps, 1) # * torch.arange(1, self.ego_fut_steps+1)
-        accelation =  gt_ego_lcf_feat[:, 2:4].unsqueeze(1).repeat(1, self.ego_fut_steps, 1) * torch.arange(1, self.ego_fut_steps+1)[None, :, None].to(vel.device) * 0.5
-        vel = vel # + accelation
+        vel = gt_ego_lcf_feat[:, :2].unsqueeze(1).repeat(1, self.ego_fut_steps, 1)
 
         fut_traj_from_velo = torch.cumsum(vel * 0.5, 1)# [0]
         gt_ego_fut_trajs = kwargs['gt_ego_fut_trajs']# [0]